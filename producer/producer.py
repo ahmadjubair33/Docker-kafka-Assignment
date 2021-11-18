7dd97b4f591205396da226f9d65a6af17e87ea8a
Jubair Ahmad <jubair.ahmed@knoldus.com>
2021-11-18 12:08:37 +0530

    first commit

---
+++ producer/producer.py
@@ -0,0 +1,37 @@
+import time 
+import json 
+import random 
+from datetime import datetime
+
+from kafka import KafkaProducer
+from data_generator import generate_message
+
+
+
+# Messages will be serialized as JSON 
+def serializer(message):
+    return json.dumps(message).encode('utf-8')
+
+
+# Kafka Producer
+producer = KafkaProducer(
+    bootstrap_servers=['kafka:9092'],
+    value_serializer=lambda x:dumps(x).encode('utf-8'),
+    api_version=(2)
+    ##value_serializer=serializer
+)
+
+
+if __name__ == '__main__':
+    # Infinite loop - runs until you kill the program
+    while True:
+        # Generate a message
+        first_kafka_topic = generate_message()
+        
+        # Send it to our 'messages' topic
+        print(f'Producing message @ {datetime.now()} | Message = {str(first_kafka_topic)}')
+        producer.send('first_topic', first_kafka_topic)
+        
+        # Sleep for a random number of seconds
+        time_to_sleep = random.randint(1, 11)
+        time.sleep(time_to_sleep)
